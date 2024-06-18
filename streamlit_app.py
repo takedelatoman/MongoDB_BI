2ddb397c4d4b23586ef974cd0adffd24cc9ede75
takedelatoman <kanashiroats@gmail.com>
2024-06-18 11:50:57 -0400

    anadiendo graficos de ventas de la tabla offers

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import random
 import pymongo
 from bson.objectid import ObjectId
 import os
@@ -8,14 +9,93 @@ import os
 # Conectar a la base de datos
 client = pymongo.MongoClient("mongodb://mongo:sfYOJDqFbGZlmmcifQxGGbXXeWLQeulo@monorail.proxy.rlwy.net:28058/springbootgraphql?authSource=admin")
 db = client['springbootgraphql']
-collection_accomodations = db['accomodations']
+
+#collection_users = db['users']
+collection_users = db.users
+collection_accomodations = db["accomodations"]
+collection_offers = db["offers"]
+
 
 # Obtener la ruta de la imagen del logo
 LOGO_PATH = os.path.join("assets", "agenciaViaje4.jpg")
 
 # Crear la aplicación Streamlit
-st.set_page_config(layout="wide", page_title="Agencia de Viajes 'URPI TOURS'")
+st.set_page_config(layout="wide", page_title="'URPI TOURS'")
 
+#borrar todo menos la 1ra
+# Obtener el primer documento de la colección
+#first_document = collection_accomodations.find_one()
+
+# Eliminar todos los documentos excepto el primero
+#collection_accomodations.delete_many({"_id": {"$ne": first_document["_id"]}})
+
+
+
+
+# Limpiar la colección 'users', excepto los dos primeros documentos
+
+#all_users = list(collection_users.find().limit(2))
+#collection_users.delete_many({})
+
+# Reinsertar los dos primeros documentos
+
+#if all_users:
+ #  collection_users.insert_many(all_users)
+
+
+#OFFERS
+# Obtener el primer documento de la colección 'offers'
+#first_offer = collection_offers.find_one()
+
+# Eliminar todos los documentos excepto el primero
+#collection_offers.delete_many({"_id": {"$ne": first_offer["_id"]}})
+
+#print("Se han eliminado todos los documentos de la colección 'offers' excepto el primero.")
+
+
+
+
+
+
+
+
+# Lista de nombres y correos electrónicos
+
+
+#nombres = [
+#    "Pedro", "Juan", "Carlos", "Miguel", "Luis", "José", "David", "Jorge",
+#    "Francisco", "Andrés", "Sergio", "Fernando", "Manuel", "Raúl", "Ricardo",
+#    "Antonio", "Pablo", "Martín", "Alejandro", "Enrique", "Alberto", "Gabriel",
+#    "Santiago", "Gonzalo", "Adrián", "Cristian", "Mario", "Diego", "Ramón", "Eduardo"
+#]
+
+# Insertar 30 usuarios únicos
+#for nombre in nombres:
+ #   user_data = {
+  #      "name": nombre,
+   #     "email": f"{nombre.lower()}@gmail.com",
+    #    "password": "$2a$10$vYEE7iek8h/VoCfWvXrm4enWT6u9onO8dh.J7xMmcNtUu.flwRiZ2",
+     #   "role": "Encargado Alojamiento"
+    #}
+    #user_data["_class"] = "com.graphl.flight.models.User"
+    #collection_users.update_one(
+     #   {"name": nombre, "email": f"{nombre.lower()}@gmail.com"},
+      #  {"$setOnInsert": user_data},
+       # upsert=True
+    #)
+
+#print("30 usuarios insertados exitosamente.")
+
+
+
+
+
+
+
+
+
+
+st.write("Usuarios insertados exitosamente")
 # Estilos
 st.markdown(
     """
@@ -86,3 +166,45 @@ if edited_df is not None and not edited_df.equals(df):
             if edited_df.at[i, col] != df.at[i, col]:
                 collection_accomodations.update_one({'_id': ObjectId(row_id)},
                                                     {"$set": {col: edited_df.at[i, col]}})
+
+
+
+
+
+
+
+
+
+
+
+
+# Función para obtener datos de la colección 'offers' y convertirlos a DataFrame
+@st.cache_data
+def get_offers_data():
+    data = list(collection_offers.find())
+    if data:
+        df = pd.DataFrame(data)
+        df['_id'] = df['_id'].astype(str)
+        return df
+    return pd.DataFrame()
+
+# Mostrar tabla de datos para 'offers'
+offers_df = get_offers_data()
+if not offers_df.empty:
+    edited_offers_df = st.experimental_data_editor(offers_df, height=300)
+else:
+    st.write("No data found in the 'offers' collection.")
+
+# Graficos para 'offers'
+if not offers_df.empty:
+    st.markdown("<h3 style='text-align: center;'>Gráficos para 'offers'</h3>", unsafe_allow_html=True)
+    
+    col1, col2, col3 = st.columns([1, 6, 1])
+    with col2:
+        offers_bar_fig = px.bar(offers_df, x='title', y='price', color='beds', title='Precio por Camas y Título')
+        st.plotly_chart(offers_bar_fig, use_container_width=True)
+    
+    col1, col2, col3 = st.columns([1, 6, 1])
+    with col2:
+        offers_scatter_fig = px.scatter(offers_df, x='state', y='title', color='price', title='Estado por Título y Precio')
+        st.plotly_chart(offers_scatter_fig, use_container_width=True)
