12c62c65569b2796a23cb15bd5e34bbf7e20e389
takedelatoman <kanashiroats@gmail.com>
2024-07-11 20:24:22 -0400

    arregla minuscula

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 import psycopg2
 import os
+import plotly.express as px
+
+# Configurar la página de Streamlit
+st.set_page_config(layout="wide", page_title="'DISTRITO 7'")
 
 # Conectar a la base de datos PostgreSQL
 def get_postgresql_connection():
@@ -15,21 +18,22 @@ def get_postgresql_connection():
     )
     return conn
 
+# Inicializar la base de datos
 def initialize_database():
     conn = get_postgresql_connection()
     cur = conn.cursor()
     
-    # Eliminar la tabla si ya existe
+    # Eliminar la tabla denuncias si ya existe
     cur.execute('DROP TABLE IF EXISTS denuncias')
     
-    # Crear la tabla
+    # Crear la nueva tabla denuncias
     cur.execute('''
         CREATE TABLE denuncias (
             id SERIAL PRIMARY KEY,
             texto TEXT,
             fecha DATE,
-            idUnidadEducativa INT,
-            imageUrl TEXT
+            "idUnidadEducativa" INT,
+            "imageUrl" TEXT
         )
     ''')
     
@@ -39,44 +43,36 @@ def initialize_database():
     
     if count == 0:
         cur.execute('''
-            INSERT INTO denuncias (texto, fecha, idUnidadEducativa, imageUrl)
+            INSERT INTO denuncias (texto, fecha, "idUnidadEducativa", "imageUrl")
             VALUES 
-                ('Denuncia por acoso escolar', '2024-06-01', 1, 'https://example.com/image1.jpg'),
-                ('Denuncia por falta de infraestructura', '2024-06-02', 2, 'https://example.com/image2.jpg'),
-                ('Denuncia por violencia física', '2024-06-03', 3, 'https://example.com/image3.jpg'),
-                ('Denuncia por falta de maestros', '2024-06-04', 4, 'https://example.com/image4.jpg'),
-                ('Denuncia por robo de materiales', '2024-06-05', 5, 'https://example.com/image5.jpg'),
-                ('Denuncia por discriminación', '2024-06-06', 1, 'https://example.com/image6.jpg'),
-                ('Denuncia por falta de agua potable', '2024-06-07', 2, 'https://example.com/image7.jpg'),
-                ('Denuncia por maltrato verbal', '2024-06-08', 3, 'https://example.com/image8.jpg'),
-                ('Denuncia por falta de electricidad', '2024-06-09', 4, 'https://example.com/image9.jpg'),
-                ('Denuncia por falta de seguridad', '2024-06-10', 5, 'https://example.com/image10.jpg')
+                ('Denuncia 1', '2024-07-01', 1, 'https://example.com/image1.jpg'),
+                ('Denuncia 2', '2024-07-02', 2, 'https://example.com/image2.jpg'),
+                ('Denuncia 3', '2024-07-03', 3, 'https://example.com/image3.jpg'),
+                ('Denuncia 4', '2024-07-04', 4, 'https://example.com/image4.jpg'),
+                ('Denuncia 5', '2024-07-05', 5, 'https://example.com/image5.jpg'),
+                ('Denuncia 6', '2024-07-06', 1, 'https://example.com/image6.jpg'),
+                ('Denuncia 7', '2024-07-07', 2, 'https://example.com/image7.jpg'),
+                ('Denuncia 8', '2024-07-08', 3, 'https://example.com/image8.jpg'),
+                ('Denuncia 9', '2024-07-09', 4, 'https://example.com/image9.jpg'),
+                ('Denuncia 10', '2024-07-10', 5, 'https://example.com/image10.jpg')
+                
         ''')
     
     conn.commit()
     cur.close()
     conn.close()
 
-def get_postgresql_data():
+# Llamar a la función de inicialización de la base de datos
+initialize_database()
+
+# Obtener los datos de PostgreSQL
+@st.cache_data
+def load_data():
     conn = get_postgresql_connection()
     query = "SELECT * FROM denuncias"
     df = pd.read_sql(query, conn)
     conn.close()
     return df
-
-# Inicializar la base de datos
-initialize_database()
-
-# Crear la aplicación Streamlit
-st.set_page_config(layout="wide", page_title="'DISTRITO 7'")
-
-# Obtener la ruta de la imagen del logo
-LOGO_PATH = os.path.join("assets", "denuncia.jpg")
-
-# Obtener los datos de PostgreSQL
-@st.cache_data
-def load_data():
-    return get_postgresql_data()
 
 df = load_data()
 
@@ -99,9 +95,9 @@ st.markdown(
 # Logo en la parte superior izquierda
 col1, col2, col3 = st.columns([1, 6, 2])  # Ajusta los anchos de las columnas según sea necesario
 with col1:
-    st.image(LOGO_PATH, width=200)
+    st.image(os.path.join("assets", "denuncia.jpg"), width=200)
 with col2:
-    st.markdown("<h1 style='text-align: center;'>DENUNCIAS EN EL DISTRITO 7 </h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;'>DENUNCIAS EN EL DISTRITO 7</h1>", unsafe_allow_html=True)
 with col3:
     st.image('https://media.dev.to/cdn-cgi/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fi%2Fwsp14jmi17ti2s1fikgw.png', width=300)
 
@@ -111,7 +107,7 @@ if not df.empty:
     with col2:
         edited_df = st.experimental_data_editor(df, height=300, width=1500)
 else:
-    st.write("No data found in the database.")
+    st.write("No se encontraron datos en la base de datos.")
 
 # Gráficos
 if not df.empty:
